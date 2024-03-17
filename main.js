ec1904025a103dc6ab595f9f0f04f47c0cfa7b24
Andrew Carolan <ajdcarolan+github@gmail.com>
2024-03-16 21:37:56 -0400

    Split into modules.

--- main.js
+++ main.js
@@ -1,36 +1,6 @@
-const throttle = (func, interval) => {
-  let lastCall = 0;
-  return function (...args) {
-    let now = new Date();
-    if (now - lastCall >= interval) {
-      func(...args);
-      lastCall = now;
-    }
-  };
-};
+import throttle from "./lib/throttle.js";
 
-const onTimeUpdate = (event) => {
-  const { currentTime, duration } = event.target;
-
-  // progress = currentTime / duration * 100;
-  // progressElement.style.width = `${progress}%`;
-
-  progress = currentTime / duration;
-  progressElement.style.transform = `scaleX(${progress})`;
-};
-
-const UPDATE_INTERVAL = 100;
-
-let progress = 0;
-let isPlaying = false;
-
-const player = document.querySelector(".player");
-
-const progressElement = player.querySelector(".progress");
-const playButton = player.querySelector("#play-button");
-const audioElement = player.querySelector("audio");
-
-playButton.addEventListener("click", (event) => {
+const startPlayback = (event) => {
   event.stopPropagation();
 
   if (!isPlaying) {
@@ -45,18 +15,37 @@ playButton.addEventListener("click", (event) => {
 
   audioElement.addEventListener(
     "timeupdate",
-    throttle(onTimeUpdate, UPDATE_INTERVAL)
+    throttle(updateProgress, UPDATE_INTERVAL)
   );
-});
+};
 
-player.addEventListener("click", (event) => {
+const updateProgress = (event) => {
+  const { currentTime, duration } = event.target;
+
+  progress = currentTime / duration;
+  progressElement.style.transform = `scaleX(${progress})`;
+};
+
+const setProgress = (event) => {
   const { offsetX: x, target } = event;
-  const { offsetWidth: width } = event.target;
+  const { offsetWidth: width } = target;
   const { duration } = audioElement;
 
   const offset = x / width;
 
-  console.log(offset * duration);
-
   audioElement.currentTime = offset * duration;
-});
+};
+
+const UPDATE_INTERVAL = 100;
+
+let progress = 0;
+let isPlaying = false;
+
+const player = document.querySelector(".player");
+
+const progressElement = player.querySelector(".progress");
+const playButton = player.querySelector("#play-button");
+const audioElement = player.querySelector("audio");
+
+playButton.addEventListener("click", startPlayback);
+player.addEventListener("click", setProgress);
