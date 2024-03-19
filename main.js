404a4bf3fba3b847eed69f06af4027794efffe6b
Andrew Carolan <acarolan@brainstation.io>
2024-03-19 11:58:34 -0400

    Persist collection of player instances in module.
    - Stop all other players when a player starts playback.
    - Add id to player instance.

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-import { createPlayer } from "./lib/player.js";
+import { init } from "./lib/player.js";
 
-let players = [];
-const playerElements = document.querySelectorAll("[data-pico]");
-
-for (const element of playerElements) {
-  const player = createPlayer(element);
-  players.push(player);
-}
-
-console.log(players);
-
-document.querySelector("#stop-all-button").addEventListener("click", () => {
-  players.forEach(({ stopPlayback }) => stopPlayback(0));
-});
-
-// const player = document.querySelector(".player");
+init();
